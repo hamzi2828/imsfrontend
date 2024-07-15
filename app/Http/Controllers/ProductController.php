611752bcaa022872496f948cb31c89b5f3e80936
hamza jahangir <hamzahashmi640@gmail.com>
2024-07-15 18:21:27 +0500

    s

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,88 +1,107 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\Product;
-    use App\Models\ProductVariation;
-    use App\Services\CategoryService;
-    use App\Services\ManufacturerService;
-    use App\Services\ProductService;
-    use Illuminate\Contracts\View\View;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Cache;
+use App\Models\Product;
+use App\Models\ProductVariation;
+use App\Services\CategoryService;
+use App\Services\ManufacturerService;
+use App\Services\ProductService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
-    class ProductController extends Controller {  
+class ProductController extends Controller
+{
 
-        public function index ( Request $request ): View {
-            $title                = 'Shop';
-            $products             = ( new ProductService() ) -> products ( $request );
-            $categories           = ( new CategoryService() ) -> category_products ();
-            $productPriceRange    = ( new ProductService() ) -> getPriceRange ();
-            $productPriceRange    = ( new ProductService() ) -> displayPriceRanges ( $productPriceRange -> minPrice, $productPriceRange -> maxPrice );
-            $manufacturers        = ( new ManufacturerService() ) -> all ();
-            return view ( 'shop.index', compact ( 'title', 'products', 'categories', 'productPriceRange', 'manufacturers' ) );
-        }
+    // public function index ( Request $request ): View {
+    //     $title                = 'Shop';
+    //     $products             = ( new ProductService() ) -> products ( $request );
 
-        public function show ( Product $product ): View {
-            $product -> load ( [ 'product_images', 'product_variations.terms.attribute', 'product_variations.terms' ] );
-            $title           = $product -> title ();
-            $relatedProducts = ( new ProductService() ) -> get_related_products ( $product );
-            $variations      = ( new ProductService() ) -> get_variations ( $product );
-            $ip              = request () -> ip ();
-            $wishlist        = Cache ::get ( "viewed_products{$ip}", [] );
-            $productId       = $product -> id;
+    //     $categories           = ( new CategoryService() ) -> category_products ();
+    //     $productPriceRange    = ( new ProductService() ) -> getPriceRange ();
+    //     $productPriceRange    = ( new ProductService() ) -> displayPriceRanges ( $productPriceRange -> minPrice, $productPriceRange -> maxPrice );
+    //     $manufacturers        = ( new ManufacturerService() ) -> all ();
+    //     return view ( 'shop.index', compact ( 'title', 'products', 'categories', 'productPriceRange', 'manufacturers' ) );
+    // }
 
-            if ( !in_array ( $productId, $wishlist ) ) {
-                $wishlist[] = $productId;
-                Cache ::put ( "viewed_products{$ip}", $wishlist, now () -> addDays ( 30 ) );
-            }
-
-            return view ( 'shop.view', compact ( 'title', 'product', 'relatedProducts', 'variations' ) );
-        }
-
-        public function quick_view ( Product $product ): View {
-            $product -> load ( [ 'product_images', 'variations' ] );
-            $title       = $product -> title ();
-            $pVariations = ( new ProductService() ) -> get_variations ( $product );
-            return view ( 'partials._quick-view', compact ( 'title', 'product', 'pVariations' ) );
-        }
-
-        public function get_variation ( Product $product, Request $request ) {
-            $query = ProductVariation ::query ();
-            $query -> where ( [ 'product_id' => $product -> id ] );
-
-            foreach ( $request -> all () as $attributeName => $termId ) {
-                $query -> whereHas ( 'terms', function ( $q ) use ( $termId ) {
-                    $q -> where ( 'term_id', $termId );
-                } );
-            }
-
-            $variation = $query -> first ();
-
-            if ( $variation ) {
-                return response () -> json ( [
-                                                 'success'   => true,
-                                                 'variation' => [
-                                                     'sku'   => $variation -> sku,
-                                                     'price' => $variation -> price,
-                                                     'stock' => $variation -> stock,
-                                                 ]
-                                             ] );
-            }
-
-            return response () -> json ( [ 'success' => false ] );
-        }
-
-        public function add_to_wishlist ( Request $request, Product $product ) {
-            $ip        = $request -> ip ();
-            $wishlist  = Cache ::get ( "wishlist_{$ip}", [] );
-            $productId = $product -> id;
-
-            if ( !in_array ( $productId, $wishlist ) ) {
-                $wishlist[] = $productId;
-                Cache ::put ( "wishlist_{$ip}", $wishlist, now () -> addDays ( 30 ) );
-            }
-
-            return $product -> id;
-        }
+    public function index(Request $request): View
+    {
+        $title = 'Shop';
+        $productServiceData = (new ProductService())->products($request);
+        $products = $productServiceData['products'];
+        $displayOutOfStockProducts = $productServiceData['displayOutOfStockProducts'];
+        $categories = (new CategoryService())->category_products();
+        $productPriceRange = (new ProductService())->getPriceRange();
+        $productPriceRange = (new ProductService())->displayPriceRanges($productPriceRange->minPrice, $productPriceRange->maxPrice);
+        $manufacturers = (new ManufacturerService())->all();
+        return view('shop.index', compact('title', 'products', 'categories', 'productPriceRange', 'manufacturers', 'displayOutOfStockProducts'));
     }
+
+    public function show(Product $product): View
+    {
+        $product->load(['product_images', 'product_variations.terms.attribute', 'product_variations.terms']);
+        $title = $product->title();
+        $relatedProducts = (new ProductService())->get_related_products($product);
+        $variations = (new ProductService())->get_variations($product);
+        $ip = request()->ip();
+        $wishlist = Cache::get("viewed_products{$ip}", []);
+        $productId = $product->id;
+
+        if (!in_array($productId, $wishlist)) {
+            $wishlist[] = $productId;
+            Cache::put("viewed_products{$ip}", $wishlist, now()->addDays(30));
+        }
+
+        return view('shop.view', compact('title', 'product', 'relatedProducts', 'variations'));
+    }
+
+    public function quick_view(Product $product): View
+    {
+        $product->load(['product_images', 'variations']);
+        $title = $product->title();
+        $pVariations = (new ProductService())->get_variations($product);
+        return view('partials._quick-view', compact('title', 'product', 'pVariations'));
+    }
+
+    public function get_variation(Product $product, Request $request)
+    {
+        $query = ProductVariation::query();
+        $query->where(['product_id' => $product->id]);
+
+        foreach ($request->all() as $attributeName => $termId) {
+            $query->whereHas('terms', function ($q) use ($termId) {
+                $q->where('term_id', $termId);
+            });
+        }
+
+        $variation = $query->first();
+
+        if ($variation) {
+            return response()->json([
+                'success' => true,
+                'variation' => [
+                    'sku' => $variation->sku,
+                    'price' => $variation->price,
+                    'stock' => $variation->stock,
+                ]
+            ]);
+        }
+
+        return response()->json(['success' => false]);
+    }
+
+    public function add_to_wishlist(Request $request, Product $product)
+    {
+        $ip = $request->ip();
+        $wishlist = Cache::get("wishlist_{$ip}", []);
+        $productId = $product->id;
+
+        if (!in_array($productId, $wishlist)) {
+            $wishlist[] = $productId;
+            Cache::put("wishlist_{$ip}", $wishlist, now()->addDays(30));
+        }
+
+        return $product->id;
+    }
+}
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -30,16 +30,17 @@
                                     <h3 class="widget-title"><span>All Categories</span></h3>
                                     <ul class="widget-body filter-items search-ul">
                                         @php $existingParams = request () -> query (); @endphp
-                                        @if(count ($categories) > 0)
-                                            @foreach($categories as $category)
+                                        @if (count($categories) > 0)
+                                            @foreach ($categories as $category)
                                                 @php $queryParams = array_merge ( $existingParams, [ 'category' => $category -> slug ] ); @endphp
-                                                <li style="{{ request ('category') == $category -> slug ? 'font-weight: 700' : '' }}">
-                                                    <a href="{{ route ('products.index', $queryParams) }}">
-                                                        @if($category -> parentCategory)
-                                                            {{ $category -> parentCategory ?-> title }}
+                                                <li
+                                                    style="{{ request('category') == $category->slug ? 'font-weight: 700' : '' }}">
+                                                    <a href="{{ route('products.index', $queryParams) }}">
+                                                        @if ($category->parentCategory)
+                                                            {{ $category->parentCategory?->title }}
                                                             <i class="w-icon-long-arrow-right"></i>
                                                         @endif
-                                                        {{ $category -> title }}
+                                                        {{ $category->title }}
                                                     </a>
                                                 </li>
                                             @endforeach
@@ -50,7 +51,8 @@
 
                                 <!-- Start of Collapsible Widget -->
                                 <div class="widget widget-collapsible">
-                                    <h3 class="widget-title {{ request () -> filled ('min-price') ? '' : 'collapsed' }}">
+                                    <h3
+                                        class="widget-title {{ request()->filled('min-price') ? '' : 'collapsed' }}">
                                         <span>Price</span>
                                     </h3>
                                     <div class="widget-body">
@@ -58,17 +60,17 @@
                                             {!! $productPriceRange !!}
                                         </ul>
                                         <form class="price-range" method="get"
-                                              action="{{ route ('products.index', request () -> all ()) }}">
+                                            action="{{ route('products.index', request()->all()) }}">
                                             <label>
                                                 <input type="number" step="any" name="min-price"
-                                                       class="min_price text-center"
-                                                       placeholder="min" value="{{ request ('min-price') }}">
+                                                    class="min_price text-center" placeholder="min"
+                                                    value="{{ request('min-price') }}">
                                             </label>
                                             <span class="delimiter">-</span>
                                             <label>
                                                 <input type="number" step="any" name="max-price"
-                                                       class="max_price text-center"
-                                                       placeholder="1max" value="{{ request ('max-price') }}">
+                                                    class="max_price text-center" placeholder="1max"
+                                                    value="{{ request('max-price') }}">
                                             </label>
                                             <button type="submit" class="btn btn-primary btn-rounded">Go</button>
                                         </form>
@@ -78,15 +80,18 @@
 
                                 <!-- Start of Collapsible Widget -->
                                 <div class="widget widget-collapsible">
-                                    <h3 class="widget-title {{ request () -> filled('manufacturer') ? '' : 'collapsed'}}">
-                                        <span>Brand</span></h3>
+                                    <h3
+                                        class="widget-title {{ request()->filled('manufacturer') ? '' : 'collapsed' }}">
+                                        <span>Brand</span>
+                                    </h3>
                                     <ul class="widget-body filter-items search-ul">
-                                        @if(count ($manufacturers) > 0)
-                                            @foreach($manufacturers as $manufacturer)
+                                        @if (count($manufacturers) > 0)
+                                            @foreach ($manufacturers as $manufacturer)
                                                 @php $queryParams = array_merge ( $existingParams, [ 'manufacturer' => $manufacturer -> slug ] ); @endphp
-                                                <li style="{{ request ('category') == $category -> slug ? 'font-weight: 700' : '' }}">
-                                                    <a href="{{ route ('products.index', $queryParams) }}">
-                                                        {{ $manufacturer -> title }}
+                                                <li
+                                                    style="{{ request('category') == $category->slug ? 'font-weight: 700' : '' }}">
+                                                    <a href="{{ route('products.index', $queryParams) }}">
+                                                        {{ $manufacturer->title }}
                                                     </a>
                                                 </li>
                                             @endforeach
@@ -103,32 +108,33 @@
 
                     <!-- Start of Shop Main Content -->
                     <div class="main-content">
-                        <form method="get" action="{{ route ('products.index', request () -> all ()) }}">
-                            @foreach(request()->all() as $key => $value)
-                                @if(!in_array($key, ['orderBy', 'per-page', 'default', 'latest', 'asc', 'desc']))
+                        <form method="get" action="{{ route('products.index', request()->all()) }}">
+                            @foreach (request()->all() as $key => $value)
+                                @if (!in_array($key, ['orderBy', 'per-page', 'default', 'latest', 'asc', 'desc']))
                                     <input type="hidden" name="{{ $key }}" value="{{ $value }}">
                                 @endif
                             @endforeach
                             <nav class="toolbox sticky-toolbox sticky-content fix-top">
                                 <div class="toolbox-left">
-                                    <a href="#" class="btn btn-primary btn-outline btn-rounded left-sidebar-toggle
+                                    <a href="#"
+                                        class="btn btn-primary btn-outline btn-rounded left-sidebar-toggle
                                         btn-icon-left d-block d-lg-none">
                                         <i class="w-icon-category"></i><span>Filters</span>
                                     </a>
                                     <div class="toolbox-item toolbox-sort select-box text-dark">
                                         <label for="orderBy">Sort By :</label>
                                         <select name="orderBy" class="form-control" id="orderBy"
-                                                onchange="this.form.submit()">
-                                            <option value="default" @selected(request ('orderBy') == 'default')>
+                                            onchange="this.form.submit()">
+                                            <option value="default" @selected(request('orderBy') == 'default')>
                                                 Default sorting
                                             </option>
-                                            <option value="latest" @selected(request ('orderBy') == 'latest')>
+                                            <option value="latest" @selected(request('orderBy') == 'latest')>
                                                 Sort by latest
                                             </option>
-                                            <option value="asc" @selected(request ('orderBy') == 'asc')>
+                                            <option value="asc" @selected(request('orderBy') == 'asc')>
                                                 Sort by price: low to high
                                             </option>
-                                            <option value="desc" @selected(request ('orderBy') == 'desc')>
+                                            <option value="desc" @selected(request('orderBy') == 'desc')>
                                                 Sort by price: high to low
                                             </option>
                                         </select>
@@ -137,41 +143,44 @@
                                 <div class="toolbox-right">
                                     <div class="toolbox-item toolbox-show select-box">
                                         <select name="per-page" class="form-control" onchange="this.form.submit()">
-                                            <option value="25" @selected(request ('per-page') == 25)>Show 25</option>
-                                            <option value="50" @selected(request ('per-page') == 50)>Show 50</option>
-                                            <option value="100" @selected(request ('per-page') == 100)>Show 100</option>
+                                            <option value="25" @selected(request('per-page') == 25)>Show 25</option>
+                                            <option value="50" @selected(request('per-page') == 50)>Show 50</option>
+                                            <option value="100" @selected(request('per-page') == 100)>Show 100</option>
                                         </select>
                                     </div>
                                     <div class="toolbox-item toolbox-layout">
-                                        <a href="{{ route ('products.index', ['view' => 'grid']) }}"
-                                           class="icon-mode-grid btn-layout {{ request ('view') == 'grid' ? 'active' : '' }}">
+                                        <a href="{{ route('products.index', ['view' => 'grid']) }}"
+                                            class="icon-mode-grid btn-layout {{ request('view') == 'grid' ? 'active' : '' }}">
                                             <i class="w-icon-grid"></i>
                                         </a>
-                                        <a href="{{ route ('products.index', ['view' => 'list']) }}"
-                                           class="icon-mode-list btn-layout {{ (request ('view') == 'list' || !request ('view')) ? 'active' : '' }}">
+                                        <a href="{{ route('products.index', ['view' => 'list']) }}"
+                                            class="icon-mode-list btn-layout {{ request('view') == 'list' || !request('view') ? 'active' : '' }}">
                                             <i class="w-icon-list"></i>
                                         </a>
                                     </div>
                                 </div>
                             </nav>
                         </form>
-                        <div class="product-wrapper row {{ (request ('view') == 'list' || !request ('view')) ? 'cols-md-1 cols-xs-2 cols-1' : 'cols-md-3 cols-sm-2 cols-2' }}">
-                            @if(count ($products) > 0)
+                        <div
+                            class="product-wrapper row {{ request('view') == 'list' || !request('view') ? 'cols-md-1 cols-xs-2 cols-1' : 'cols-md-3 cols-sm-2 cols-2' }}">
                             @if (count($products) > 0)
                                 @foreach ($products as $product)
-                                    @if ($product->available_quantity() == 0 || $product->available_quantity() > 0   )
-                                        @if (request('view') == 'list' || !request('view'))
-                                            <x-products-list :product="$product"></x-products-list>
-                                        @else
-                                            <x-products-card :product="$product"></x-products-card>
-                                        @endif
+                                    @if ($product->available_quantity() == 0 && $displayOutOfStockProducts != 1)
+                                        @continue
+                                        <!-- Skip to the next iteration if out of stock products should not be displayed -->
+                                    @endif
+
+                                    @if (request('view') == 'list' || !request('view'))
+                                        <x-products-list :product="$product"></x-products-list>
+                                    @else
+                                        <x-products-card :product="$product"></x-products-card>
                                     @endif
                                 @endforeach
-                        @endif
                             @endif
+
                         </div>
                         <div class="toolbox toolbox-pagination justify-content-between">
-                            {!! $products -> onEachSide(5) -> appends(request() -> query()) -> links('shop.pagination') !!}
+                            {!! $products->onEachSide(5)->appends(request()->query())->links('shop.pagination') !!}
                         </div>
                     </div>
                     <!-- End of Shop Main Content -->
