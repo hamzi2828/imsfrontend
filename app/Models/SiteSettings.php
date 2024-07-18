9fb9a8d3ca3f0865f97e02d6adbebea1970941b9
hamza jahangir <hamzahashmi640@gmail.com>
2024-07-18 14:56:05 +0500

    new changes

--- app/Models/SiteSettings.php
+++ app/Models/SiteSettings.php
@@ -5,24 +5,13 @@
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
 
-    // class SiteSettings extends Model {
-    //     use HasFactory;
-
-    //     protected $guarded = [];
-    //     protected $table   = 'site_settings';
-
-    //     public function getSettingsAttribute ( $value ) {
-    //         return json_decode ( $value );
-    //     }
-    // }
     class SiteSettings extends Model {
         use HasFactory;
-    
+
         protected $guarded = [];
         protected $table   = 'site_settings';
-    
-        public function getSettingsAttribute($value) {
-            return json_decode($value, true); // Decode as associative array
+
+        public function getSettingsAttribute ( $value ) {
+            return json_decode ( $value );
         }
     }
-    
