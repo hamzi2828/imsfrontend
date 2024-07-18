c804371987ff01a7553bf917e6bdaade0e109739
hamza jahangir <hamzahashmi640@gmail.com>
2024-07-18 18:18:54 +0500

    Reapply "Reapply "new changes""
    This reverts commit c5f99e1b2b3686e559edfa6c9e56984f45f75394.

--- resources/views/components/products-card.blade.php
+++ resources/views/components/products-card.blade.php
@@ -5,34 +5,12 @@
                 <img src="{{ serverPath ($product -> image) }}" alt="{{ $product -> title() }}" width="300"
                      height="338" />
             </a>
-            {{-- <div class="product-action-horizontal">
+            <div class="product-action-horizontal">
                 <a href="javascript:void(0)"
                    onclick="initProductQuickView('{{ route ('products.quick-view', ['product' => $product -> slug]) }}')"
                    class="btn-product-icon w-icon-search"
                    title="Quick View"></a>
-            </div> --}}
-
-            <div class="product-action-vertical">
-                @if($product -> has_variations == '1')
-                        <a href="javascript:void(0)" class="btn-product-icon w-icon-search"
-                        onclick="initProductQuickView('{{ route ('products.quick-view', ['product' => $product -> slug]) }}')"
-                        title="Quick View"></a>
-                    @else
-                        <a href="javascript:void(0)" class="btn-product-icon w-icon-cart"
-                        onclick="addToCart(this, '{{ route ('cart.store', ['product' => $product -> slug]) }}', {{ $product -> available_quantity() }})"
-                        title="Add to cart"></a>
-                        
-                        <a href="javascript:void(0)" id="wishlist-{{ $product -> id }}"
-                        onclick="addToWishList('{{ route ('products.add-to-wishlist', ['product' => $product -> slug]) }}')"
-                        class="btn-product-icon btn-wishlist {{ isWishList($product -> id) ? 'w-icon-heart-full' : 'w-icon-heart' }}"
-                        title="Add to wishlist"></a>
-                        
-                        <a href="javascript:void(0)" class="btn-product-icon w-icon-search"
-                        onclick="initProductQuickView('{{ route ('products.quick-view', ['product' => $product -> slug]) }}')"
-                        title="Quick View"></a>
-                    @endif
             </div>
-            
         </figure>
         <div class="product-details">
             <div class="product-cat">
